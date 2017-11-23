38b98ead22f822c6b8b63ae68e43292a3727aeed
Ivange Larry <ivangelarry@gmail.com>
2017-11-23 05:52:48 +0100

    BotController.java: Extract string literals and make them final constansts

--- src/main/java/org/openmrs/bot/openmrsgcibot/controller/BotController.java
+++ src/main/java/org/openmrs/bot/openmrsgcibot/controller/BotController.java
@@ -19,6 +19,18 @@ public class BotController {
     @Autowired
     private Config config;
 
+    private static final String EventTypeComment = "comment";
+
+    private static final String EventTypeSubmit = "submit";
+
+    private static final String EventTypeNeedsWork = "needswork";
+
+    private static final String EventTypeAbandon = "abandon";
+
+    private static final String EventTypeClaim = "claim";
+
+    private static final String EventTypeApprove = "approve";
+
     @RequestMapping(value = "notifications", method = RequestMethod.POST)
     public ResponseEntity<String> receiveNotifications(@RequestBody Message message) throws JsonProcessingException {
 
@@ -46,17 +58,17 @@ public class BotController {
         final String template = "<OpenMRSGciBot> New from Google Code-in: " + message.getAuthor() + " {0} " + message.getTask_definition_name() + ". See " + message.getTask_instance_url();
 
         switch (message.getEvent_type()) {
-            case "comment":
+            case EventTypeComment:
                 return MessageFormat.format(template, " commented on ");
-            case "submit":
+            case EventTypeSubmit:
                 return MessageFormat.format(template, " submitted task for ");
-            case "needswork":
+            case EventTypeNeedsWork:
                 return MessageFormat.format(template, " requested for more work on ");
-            case "abandon":
+            case EventTypeAbandon:
                 return MessageFormat.format(template, " abandoned task ");
-            case "claim":
+            case EventTypeClaim:
                 return MessageFormat.format(template, " started working on ");
-            case "approve":
+            case EventTypeApprove:
                 return MessageFormat.format(template, " accepted a submission for ");
             default:
                 return MessageFormat.format(template, " triggered unknown event for ");
