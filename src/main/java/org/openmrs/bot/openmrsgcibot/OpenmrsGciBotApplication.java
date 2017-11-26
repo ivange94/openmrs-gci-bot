60cad3509b923d2bc37b4524240ff0fe4ee825db
Ivange Larry <ivangelarry@gmail.com>
2017-11-26 06:26:58 +0100

    Remove unused import

--- src/main/java/org/openmrs/bot/openmrsgcibot/OpenmrsGciBotApplication.java
+++ src/main/java/org/openmrs/bot/openmrsgcibot/OpenmrsGciBotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
